2e6eec5ca4e76adf89c3bcf2ded9f63319810161
Alessandro Fulgini <fuljo97@gmail.com>
2023-02-23 11:23:39 +0100

    feat(admin): upload and delete slide decks from UI
    Plus a minor refactoring of admin.js

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,6 +1,7 @@
 import { doc, updateDoc } from 'firebase/firestore';
 import { getAuth, signOut, onAuthStateChanged } from 'firebase/auth';
-import { ref, listAll } from 'firebase/storage';
+import { ref, listAll, uploadBytes, deleteObject } from 'firebase/storage';
+import { Modal } from 'bootstrap';
 
 import { firebaseConfig } from './common';
 import { ViewerApp } from './viewer';
@@ -19,6 +20,16 @@ class AdminApp extends ViewerApp {
     toolbarContainer;
     /** @type {HTMLInputElement} */
     deckNameInput;
+    /** @type {HTMLInputElement} */
+    deckFileInput;
+    /** @type {HTMLButtonElement} */
+    uploadButton;
+    /** @type {HTMLButtonElement} */
+    submitUploadButton;
+    /** @type {HTMLButtonElement} */
+    deleteButton;
+    /** @type {HTMLButtonElement} */
+    confirmDeleteButton;
     /** @type {HTMLInputElement} */
     pageNumberInput;
     /** @type {HTMLElement} */
@@ -40,7 +51,7 @@ class AdminApp extends ViewerApp {
     /** @type {boolean} */
     useWakeLock;
 
-    constructor(viewerContainer, toolbarContainer, deckNameInput, pageNumberInput, pagesCountSpan, prevPageButton, nextPageButton, emailField, loginButton, logoutButton, firebaseConfig, useWakeLock) {
+    constructor(viewerContainer, toolbarContainer, firebaseConfig, useWakeLock) {
         super(viewerContainer, firebaseConfig, useWakeLock);
 
         // Firestore references
@@ -49,17 +60,29 @@ class AdminApp extends ViewerApp {
         this.auth = getAuth(this.app);
 
         this.toolbarContainer = toolbarContainer;
-        this.deckNameInput = deckNameInput;
-        this.pageNumberInput = pageNumberInput;
-        this.pagesCountSpan = pagesCountSpan;
-        this.prevPageButton = prevPageButton;
-        this.nextPageButton = nextPageButton;
-        this.emailField = emailField;
-        this.loginButton = loginButton;
-        this.logoutButton = logoutButton;
+        this.deckNameInput = document.getElementById("deckName");
+        this.uploadButton = document.getElementById("upload");
+        this.uploadDialog = new Modal(document.getElementById("uploadModal"));
+        this.deckFileInput = document.getElementById("deckFile");
+        this.submitUploadButton = document.getElementById("submitUpload");
+        this.deleteButton = document.getElementById("delete");
+        this.deleteDialog = new Modal(document.getElementById("deleteModal"));
+        this.confirmDeleteButton = document.getElementById("confirmDelete");
+        this.pageNumberInput = document.getElementById("pageNumber");
+        this.pagesCountSpan = document.getElementById("pagesCount");
+        this.prevPageButton = document.getElementById("prevPage");
+        this.nextPageButton = document.getElementById("nextPage");
+        this.emailField = document.getElementById("email");
+        this.loginButton = document.getElementById("login");
+        this.logoutButton = document.getElementById("logout");
 
         // Event listeners
         this.deckNameInput.addEventListener("change", this._onDeckNameInputChange.bind(this));
+        this.uploadDialog._element.addEventListener("hidden.bs.modal", this._onUploadDialogClose.bind(this));
+        this.deckFileInput.addEventListener("change", this._onDeckFileInputChange.bind(this));
+        this.submitUploadButton.addEventListener("click", this._onUploadDeck.bind(this));
+        this.deleteDialog._element.addEventListener("hidden.bs.modal", this._onDeleteDialogClose.bind(this));
+        this.confirmDeleteButton.addEventListener("click", this._onDeleteDeck.bind(this));
         this.pageNumberInput.addEventListener("change", this._onPageNumberInputChange.bind(this));
         this.prevPageButton.addEventListener("click", this._onPageChangeButton.bind(this, -1));
         this.nextPageButton.addEventListener("click", this._onPageChangeButton.bind(this, +1));
@@ -168,6 +191,8 @@ class AdminApp extends ViewerApp {
             this.logoutButton.hidden = false;
 
             this.deckNameInput.disabled = false;
+            this.uploadButton.disabled = false;
+            this.deleteButton.disabled = false;
             this.pageNumberInput.disabled = false;
             this.prevPageButton.disabled = false;
             this.nextPageButton.disabled = false;
@@ -178,6 +203,8 @@ class AdminApp extends ViewerApp {
             this.logoutButton.hidden = true;
 
             this.deckNameInput.disabled = true;
+            this.uploadButton.disabled = true;
+            this.deleteButton.disabled = true;
             this.pageNumberInput.disabled = true;
             this.prevPageButton.disabled = true;
             this.nextPageButton.disabled = true;
@@ -191,6 +218,105 @@ class AdminApp extends ViewerApp {
     _onDeckNameInputChange(event) {
         const deckName = event.target.value;
         this.updateRemoteState({ deckName: deckName });
+    }
+
+    /**
+     * Handle deck upload input change.
+     * @param {*} event 
+     */
+    _onDeckFileInputChange(_event) {
+        document.getElementById("uploadError").hidden = true;
+        document.getElementById("uploadSuccess").hidden = true;
+    }
+
+    /**
+     * Reset the state of the upload dialog.
+     * @param {Event} event
+     */
+    _onUploadDialogClose(_event) {
+        this.deckFileInput.value = "";
+        document.getElementById("uploadError").hidden = true;
+        document.getElementById("uploadSuccess").hidden = true;
+    }
+
+    /**
+     * Handle uploading a slide deck.
+     * @param {Event} event
+     */
+    _onUploadDeck(event) {
+        event.preventDefault();
+        let errorAlert = document.getElementById("uploadError");
+        const successAlert = document.getElementById("uploadSuccess");
+        errorAlert.hidden = true;
+        successAlert.hidden = true;
+
+        const file = this.deckFileInput.files[0];
+        if (!file) {
+            console.error("No file selected.");
+            errorAlert.textContent = "No file selected.";
+            errorAlert.hidden = false;
+        }
+        if (file.type !== "application/pdf") {
+            console.error("File must be a PDF.");
+            errorAlert.textContent = "File must be a PDF.";
+            errorAlert.hidden = false;
+        }
+
+        const deckName = file.name.replace(/\.pdf$/, "");
+        const deckRef = ref(this.storage, `decks/${deckName}.pdf`);
+        uploadBytes(deckRef, file).then((_snapshot) => {
+            if (this.deckNameInput.querySelector(`option[value="${deckName}"]`) == null) {
+                const option = document.createElement("option");
+                option.value = deckName;
+                option.textContent = deckName;
+                this.deckNameInput.appendChild(option);
+                successAlert.textContent = `Uploaded ${deckName}.`;
+                this.deckFileInput.value = "";
+            } else {
+                successAlert.textContent = `Updated ${deckName}.`;
+            }
+            successAlert.hidden = false;
+        }).catch((error) => {
+            console.error("Error uploading file: ", error);
+            let errorAlert = document.getElementById("uploadError");
+            errorAlert.textContent = `Error uploading file: ${error}`;
+            errorAlert.hidden = false;
+        });
+    }
+
+    /**
+     * Reset the state of the delete dialog.
+     * @param {Event} event
+     */
+    _onDeleteDialogClose(_event) {
+        document.getElementById("deleteError").hidden = true;
+        document.getElementById("deleteSuccess").hidden = true;
+    }
+
+    /**
+     * Handle deleting a slide deck.
+     * @param {Event} event 
+     */
+    _onDeleteDeck(event) {
+        event.preventDefault();
+        const deckName = this.deckNameInput.value;
+        if (deckName) {
+            const deckRef = ref(this.storage, `decks/${deckName}.pdf`);
+            deleteObject(deckRef).then(() => {
+                this.deckNameInput.querySelector(`option[value="${deckName}"]`).remove();
+                this.deckNameInput.value = "";
+                this.pagesCountSpan.textContent = "";
+                this.pageNumberInput.value = "";
+                const deleteSuccessAlert = document.getElementById("deleteSuccess");
+                deleteSuccessAlert.textContent = `Deleted ${deckName}.`;
+                deleteSuccessAlert.hidden = false;
+            }).catch((error) => {
+                console.error("Error deleting file: ", error);
+                const deleteErrorAlert = document.getElementById("deleteError");
+                deleteErrorAlert.textContent = `Error deleting file: ${error}`;
+                deleteErrorAlert.hidden = false;
+            });
+        }
     }
 
     /**
@@ -237,14 +363,6 @@ window.addEventListener("DOMContentLoaded", () => {
     new AdminApp(
         document.getElementById("viewerContainer"),
         document.getElementById("toolbarContainer"),
-        document.getElementById("deckName"),
-        document.getElementById("pageNumber"),
-        document.getElementById("pagesCount"),
-        document.getElementById("prevPage"),
-        document.getElementById("nextPage"),
-        document.getElementById("email"),
-        document.getElementById("login"),
-        document.getElementById("logout"),
         firebaseConfig,
         useWakeLock,
     );
